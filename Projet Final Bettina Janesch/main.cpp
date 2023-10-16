04a2e4e5a69626450f8a1411add9aca0f4bce8ef
Janesch Bettina-Sarah <e.bsjanesch@etu.cvm.qc.ca>
2023-10-16 14:08:34 -0400

    menu

---
+++ Projet Final Bettina Janesch/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <conio.h> //getch
+#include <stdlib.h> //system cls
+
+using namespace std;
+
+const string Bibliotheque = "Bibliothèque de la MRC St-Clin-de-banlieue";
+
+void main(void)
+{
+	setlocale(LC_CTYPE, "fr-CA");
+	int OptionMenu;
+	do
+	{
+		cout << "\t********\n";
+		cout << Bibliotheque;
+		cout << "\n\t********\n";
+		cout << "\n\n****** MENU ******\n\t1. Nouveau livre\n\t2. Nouveau client\n\t3. Dossier du client\n\t4. Location de livre"
+			"\n\t5. Retour des livres\n\t6. Liste des livres prêtés\n\t7. Liste des clients en retard\n\t8. Quitter";
+		OptionMenu = _getche();
+		switch (OptionMenu)
+		{
+		case '1':
+			cout << "hello";
+			break;
+		case '2':
+			break;
+		case '3':
+			break;
+		case '4':
+			break;
+		case '5':
+			break;
+		case '6':
+			break;
+		case '7':
+			break;
+		case '8':
+			system("cls");
+			_getch();
+			break;
+
+		} 
+	}
+	while (OptionMenu != '8');
+
+}
--- Projet Final Bettina Janesch/utils.cpp
+++ Projet Final Bettina Janesch/utils.cpp
@@ -45,35 +45,35 @@ Date_s AjouterJours(int NbJours, Date_s DateDepart) {
 	DateMAJ.Jour = UneDate.tm_mday;
 	return DateMAJ;
 }
-
+ 
 int NombreJours(Date_s Date1, Date_s Date2) {
-	time_t Date1Secondes;
-	time_t Date2Secondes;
+time_t Date1Secondes;
+time_t Date2Secondes;
 
-	// ***************************
-	// Convertir Date1 en secondes
-	// ***************************
+// ***************************
+// Convertir Date1 en secondes
+// ***************************
 
-	struct tm UneDate1 = {};
-	UneDate1.tm_year = Date1.Annee - 1900;
-	UneDate1.tm_mon = Date1.Mois + 1;
-	UneDate1.tm_mday = Date1.Jour;
-
-	// Convertir structure de date en date_secondes (nb secondes de puis 1900-1-1.0
-	Date1Secondes = mktime(&UneDate1);
-
-	// ***************************
-	// Convertir Date2 en secondes
-	// ***************************
-
-	struct tm UneDate2 = {};
-	UneDate2.tm_year = Date2.Annee - 1900;
-	UneDate2.tm_mon = Date2.Mois + 1;
-	UneDate2.tm_mday = Date2.Jour;
-
-	// Convertir structure de date en date_secondes (nb secondes de puis 1900-1-1.0
-	Date2Secondes = mktime(&UneDate2);
-
-	return (Date2Secondes - Date1Secondes) / 24 / 60 / 60;
+struct tm UneDate1 = {};
+UneDate1.tm_year = Date1.Annee - 1900;
+UneDate1.tm_mon = Date1.Mois + 1;
+UneDate1.tm_mday = Date1.Jour;
+ 
+// Convertir structure de date en date_secondes (nb secondes de puis 1900-1-1.0
+Date1Secondes = mktime(&UneDate1);
+ 
+// ***************************
+// Convertir Date2 en secondes
+// ***************************
+ 
+struct tm UneDate2 = {};
+UneDate2.tm_year = Date2.Annee - 1900;
+UneDate2.tm_mon = Date2.Mois + 1;
+UneDate2.tm_mday = Date2.Jour;
+ 
+// Convertir structure de date en date_secondes (nb secondes de puis 1900-1-1.0
+Date2Secondes = mktime(&UneDate2);
+ 
+return (Date2Secondes - Date1Secondes) / 24 / 60 / 60;
 }
 
